cd10cdf4e991f6a1ad87d52d84bd5092c56b97b0
Janna <janny_b@hotmail.com>
2021-12-03 20:20:54 -0300

    Adicionei rota no meu servidor

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "Olá Mundo, este é meu novo repositório clonado!" });
+})
+
+/*app.get('/ola', (req, res) => {
+    res.status(200).send({ message: "Oi eu sou a rota Olá!" });
+})*/
+
+app.listen(4001, () => {
+    console.log("Api rodando na porta 4001");
+})
